1d0c4d75e7e57b376ab3c7f12b295bff12dc44f2
Thomas Santana <mailleux@gmail.com>
2008-12-18 19:50:00 +0000

    Added Ant build for automatically creating a dist. Fixed typo in sample

---
+++ scripts/vcc.bat
@@ -0,0 +1,3 @@
+@echo off
+
+java -cp "bin\scala-swing.jar;bin\scala-library.jar;bin\vcc-{VERSION}.jar" vcc.Main
---
+++ scripts/vcc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp "bin/scala-swing.jar:bin/scala-library.jar:bin/vcc-0.10.jar" vcc.Main
