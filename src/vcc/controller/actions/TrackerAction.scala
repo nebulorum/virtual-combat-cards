628e13388dc7889cc45677d70c9531089767c5a3
Thomas Santana <mailleux@gmail.com>
2009-01-28 22:23:49 +0000

    First adjust to make transaction aware.

--- src/vcc/controller/actions/TrackerAction.scala
+++ src/vcc/controller/actions/TrackerAction.scala
@@ -1,35 +1,94 @@
 //$Id$
 package vcc.controller.actions
 
+import vcc.controller.transaction.TransactionDescription
 import vcc.model.CombatantTemplate
 
 class TrackerAction {
 }
 
+abstract class TransactionalAction extends TransactionDescription
+
+abstract class QueryAction
+
 // Builing the combat grid
-case class AddCombatant(template:CombatantTemplate)
-case class Enumerate()
-case class ClearCombatants(all:Boolean)
-case class StartCombat(order:Seq[Symbol])
-case class EndCombat()
 
-case class SetComment(to:Symbol,text:String)
+abstract class EncounterAction extends TransactionalAction
 
-case class ApplyDamage(to:Symbol,damage:Int)
-case class SetTemporaryHP(to:Symbol,temphp:Int)
-case class HealDamage(to:Symbol,heal:Int)
-case class FailDeathSave(to:Symbol)
+case class StartTransaction(desc:String)
+case class EndTransaction(desc:String)
+
+case class Undo() 
+case class Redo()
+
+case class AddCombatant(template:CombatantTemplate) extends EncounterAction {
+  def description():String = "Add combatant"
+}
+case class Enumerate() extends QueryAction
+
+case class ClearCombatants(all:Boolean) extends EncounterAction {
+  def description():String = "Clear "+(if(all) "all combatants" else "monsters")
+}
+
+case class StartCombat(order:Seq[Symbol]) extends EncounterAction {
+  def description():String = "Start combat, with order: "+order
+}
+
+case class EndCombat() extends EncounterAction {
+  def description():String = "End combat"
+}
+
+abstract class StateChangeAction extends TransactionalAction
+
+case class SetComment(to:Symbol,text:String) extends TransactionalAction {
+  def description():String = "Comment of "+to.name+ "has changed"
+}
+
+abstract class HealthChangeAction extends TransactionalAction
+
+case class ApplyDamage(to:Symbol,damage:Int) extends HealthChangeAction {
+  def description():String = to.name + "takens "+damage + " hitpoints of damage"
+}
+
+case class SetTemporaryHP(to:Symbol,temphp:Int) extends HealthChangeAction {
+  def description():String = to.name + " recieves "+temphp + " of temporary hitpoints"
+}
+
+case class HealDamage(to:Symbol,heal:Int) extends HealthChangeAction {
+  def description():String = to.name + " recovers "+heal + " hitpoints"
+}
+
+case class FailDeathSave(to:Symbol) extends HealthChangeAction {
+  def description():String = to.name + "fails save versus death"
+}
 
 
-case class StartRound(who:Symbol)
-case class EndRound(who:Symbol)
-case class Ready(who:Symbol)
-case class Delay(who:Symbol)
-case class MoveUp(who:Symbol)
-case class ExecuteReady(who:Symbol)
+/**
+ * Master class for all initiave based actions
+ */
+abstract case class InitiativeAction() extends TransactionalAction
+
+case class StartRound(who:Symbol) extends InitiativeAction {
+  def description():String = "Start of "+ who.name + "'s round"
+}
+case class EndRound(who:Symbol) extends InitiativeAction {
+  def description():String = "End of "+ who.name + "'s round"
+}
+case class Ready(who:Symbol) extends InitiativeAction {
+  def description():String = "Ready action by"+ who.name
+}
+case class Delay(who:Symbol) extends InitiativeAction {
+  def description():String = "Delay action by "+ who.name
+}
+case class MoveUp(who:Symbol) extends InitiativeAction {
+  def description():String =  who.name + "has moved up round"
+}
+case class ExecuteReady(who:Symbol) extends InitiativeAction {
+  def description():String = who.name + " executed a ready action"
+}
 
 case class LogError(msg:String)
 
 
-case class QueryCombatantMap[T](func:(TrackerCombatant=>T))
+case class QueryCombatantMap[T](func:(TrackerCombatant=>T)) extends QueryAction
 
