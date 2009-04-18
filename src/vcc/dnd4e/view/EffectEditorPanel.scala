78c3629f82a69c4277bea995b38ad7a887493377
Thomas Santana <mailleux@gmail.com>
2009-04-18 01:47:58 +0000

    Implement memory of effect panel options per combatant name.

--- src/vcc/dnd4e/view/EffectEditorPanel.scala
+++ src/vcc/dnd4e/view/EffectEditorPanel.scala
@@ -10,6 +10,9 @@ import vcc.dnd4e.model.Effect
 
 class EffectEditorPanel(tracker: Actor) extends MigPanel("fillx,hidemode 3") with SequenceView[ViewCombatant] with ContextualView[ViewCombatant]{
   
+  private val memory= scala.collection.mutable.Map.empty[String,List[EffectEditor.StateMemento]]
+  private var lastActiveKey:String=null
+  
   class ActiveCombatant(val c:ViewCombatant) {
     override def toString()= c.id.name + " - "+c.name
   }
@@ -21,7 +24,6 @@ class EffectEditorPanel(tracker: Actor) extends MigPanel("fillx,hidemode 3") wit
     new EffectEditor(this),
     //new EffectEditor(this),
     new EffectEditor(this))
-
   
   border= javax.swing.BorderFactory.createTitledBorder("Effect Creation")
   add(new Label("Source:"),"span,split 2")
@@ -30,19 +32,20 @@ class EffectEditorPanel(tracker: Actor) extends MigPanel("fillx,hidemode 3") wit
 	  addSeparator("Effect")
 	  add(efp,"span 2,wrap,grow x")
   }
- 
   
   // Set and handle reaction to changing the active characters
   listenTo(activeCombo.selection)
   reactions += {
     case event.SelectionChanged(`activeCombo`)=>
-      println(activeCombo.selection.index )
+      switchActive(activeCombo.selection.item.c.name)
+/*      println(activeCombo.selection.index )
       println(activeCombo.selection.item.c.name)
       for(efp<-efpl)
     	  println(efp.saveMemento())
   //TEST
   efpl(0).restoreMemento(EffectEditor.StateMemento(0,("I set this"),4,true))
   efpl(1).restoreMemento(EffectEditor.StateMemento(1,('G,true),3,false))
+  */
   }
   
   def updateSequence(seq:Seq[ViewCombatant]):Unit= {
@@ -55,6 +58,7 @@ class EffectEditorPanel(tracker: Actor) extends MigPanel("fillx,hidemode 3") wit
       val lid=seq.map(c=>{c.id.name})
       for(efp<-efpl) efp.setSequence(lid)
     }
+    switchActive(activeCombo.selection.item.c.name)
   }
 
   def changeContext(nctx:Option[ViewCombatant]) {
@@ -66,5 +70,20 @@ class EffectEditorPanel(tracker: Actor) extends MigPanel("fillx,hidemode 3") wit
     val cond=subPanel.generateEffect(source,context)
     val duration=durOption.generate(source,context)
     tracker ! request.AddEffect(context.id,Effect(source.id,cond,beneficial,duration))
+  }
+  
+  /**
+   * Store data on the effect memory
+   */
+  def switchActive(nkey:String) {
+    // If we have a key store it
+    if(lastActiveKey!=null) {
+      memory(lastActiveKey)=efpl.map(epl=>epl.saveMemento())
+    }
+    lastActiveKey=nkey
+    // Restore the previous mementos if they exit
+    if(memory.contains(nkey)) {
+      efpl.zip(memory(nkey)).map(x=>x._1.restoreMemento(x._2))
+    }
   }
 }
