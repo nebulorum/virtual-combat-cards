d994f58d2ea7f3e6733b8845350edeb7191d733c
Thomas Santana <mailleux@gmail.com>
2009-05-01 19:13:59 +0000

    Change script to work on Mac OS X 10.5

--- scripts/vcc.sh
+++ scripts/vcc.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+#$Id$
 
-java -cp "bin/scala-swing.jar:bin/scala-library.jar:bin/miglayout-3.6.2-swing.jar:bin/vcc.jar" vcc.Main
+OS=`uname` 
+if [ $OS == 'Darwin' ]; then
+	echo "Running on a MAC"
+	JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java"
+	if [ ! -f $JAVA ]; then
+		echo "Can't find a suitable JRE, need version 1.6.0" 
+		exit 1
+	fi
+else
+	echo "Running on other Unix"
+	JAVA=java
+fi
+$JAVA -cp "bin/scala-swing.jar:bin/scala-library.jar:bin/miglayout-3.6.2-swing.jar:bin/vcc.jar" vcc.Main
