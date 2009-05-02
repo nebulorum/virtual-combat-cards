2ad8e11e63a000502c7d831ee95679246172509c
Thomas Santana <mailleux@gmail.com>
2009-05-02 02:46:36 +0000

    Add size parameter parameter for the Mac (issue 78)

--- scripts/vcc.sh
+++ scripts/vcc.sh
@@ -2,8 +2,9 @@
 #$Id$
 
 OS=`uname` 
-if [ $OS == 'Darwin' ]; then
+if [ $OS = 'Darwin' ]; then
 	echo "Running on a MAC"
+	ARGS="-Dvcc.view.lpanel.width=460 -Dvcc.view.efp.max=2"
 	JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/bin/java"
 	if [ ! -f $JAVA ]; then
 		echo "Can't find a suitable JRE, need version 1.6.0" 
@@ -13,4 +14,5 @@ else
 	echo "Running on other Unix"
 	JAVA=java
 fi
-$JAVA -cp "bin/scala-swing.jar:bin/scala-library.jar:bin/miglayout-3.6.2-swing.jar:bin/vcc.jar" vcc.Main
+CLASSPATH="bin/scala-swing.jar:bin/scala-library.jar:bin/miglayout-3.6.2-swing.jar:bin/vcc.jar"
+$JAVA -cp $CLASSPATH $ARGS vcc.Main
