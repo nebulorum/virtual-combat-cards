21dc1875732fdc5054554832560007271f0f0bb7
Thomas Santana <mailleux@gmail.com>
2009-05-02 02:49:43 +0000

    Moved to version 0.92.2

--- src/vcc/dnd4e/BootStrap.scala
+++ src/vcc/dnd4e/BootStrap.scala
@@ -2,5 +2,12 @@
 package vcc.dnd4e
 
 object BootStrap {
-	val version="0.92.1"
+  
+  val version="0.92.2"
+  
+  def getPropertyAsInt(name:String,default:Int):Int = try {
+    System.getProperties.getProperty(name).toInt
+  } catch {
+    case _ => default
+  }
 }
