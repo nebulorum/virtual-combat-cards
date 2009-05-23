0def0f4daab72b9f65e44a60ac5a093dd7cd1be5
Thomas Santana <mailleux@gmail.com>
2009-05-23 13:14:46 +0000

    Update version for version 0.92.4, fix of issue #91 (not 120)

--- src/vcc/dnd4e/BootStrap.scala
+++ src/vcc/dnd4e/BootStrap.scala
@@ -3,7 +3,7 @@ package vcc.dnd4e
 
 object BootStrap {
   
-  val version="0.92.3"
+  val version="0.92.4"
   
   def getPropertyAsInt(name:String,default:Int):Int = try {
     System.getProperties.getProperty(name).toInt
