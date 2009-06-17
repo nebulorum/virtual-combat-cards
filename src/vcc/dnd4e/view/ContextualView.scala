9b40643a51cc3058932c6503c1af96e8de3a01bc
Thomas Santana <mailleux@gmail.com>
2009-06-17 02:31:26 +0000

    Added isInContext

--- src/vcc/dnd4e/view/ContextualView.scala
+++ src/vcc/dnd4e/view/ContextualView.scala
@@ -20,6 +20,7 @@ package vcc.dnd4e.view
 class NotInContextException extends Exception("Not in Context")
 
 trait ContextualView[T] {
+  
   protected var _context:Option[T]=None
   
   def context_=(v:Option[T])= {
@@ -27,6 +28,10 @@ trait ContextualView[T] {
     changeContext(v); 
     _context=v; 
   }
+  
   def context:T=_context match { case Some(v)=>v; case None => throw new NotInContextException()}
+  
+  def isInContext:Boolean = _context.isDefined
+  
   def changeContext(context:Option[T]) 
 }
