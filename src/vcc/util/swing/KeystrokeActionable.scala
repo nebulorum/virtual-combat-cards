58ecfd83d46bd49622c1a705941911e6d9e49f69
Thomas Santana <mailleux@gmail.com>
2009-06-19 02:28:09 +0000

    Added flexible keystroke binding

--- src/vcc/util/swing/KeystrokeActionable.scala
+++ src/vcc/util/swing/KeystrokeActionable.scala
@@ -20,6 +20,12 @@ package vcc.util.swing
 import scala.swing.{Component,Action}
 import javax.swing.KeyStroke
 
+object FocusCondition extends Enumeration {
+    val WhenAncestorFocused = Value(javax.swing.JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+    val WhenFocused = Value(javax.swing.JComponent.WHEN_FOCUSED)
+    val WhenWindowFocused = Value(javax.swing.JComponent.WHEN_IN_FOCUSED_WINDOW) 
+}
+
 /**
  * Provides KeyStroke binding to actions.
  */
@@ -28,23 +34,34 @@ trait KeystrokeActionable {
   
   /**
    * Associate a keystroke to an action
+   * @param focus What is the focus condition to associated
+   * @param keystroke String description of the key stroke to associated
+   * @param action The action to be executed
+   * @return True if the keystroke was associated 
+   */
+  def bindKeystrokeAction(focus: FocusCondition.Value,keystroke: String, action:Action):Boolean = {
+    val ks=KeyStroke.getKeyStroke(keystroke)
+    bindKeystrokeAction(focus,ks,action)
+  }
+  
+  /**
+   * Associate a keystroke to an action
    * @param keystroke String description of the key stroke to associated
    * @param action The action to be executed
    * @return True if the keystroke was associated 
    */
   def bindKeystrokeAction(keystroke: String, action:Action):Boolean = {
-    val ks=KeyStroke.getKeyStroke(keystroke)
-    bindKeystrokeAction(ks,action)
+    bindKeystrokeAction(FocusCondition.WhenFocused, keystroke, action)
   }
-  
   /**
    * Associate a keystroke to an action
+   * @param focus What is the focus condition to associated
    * @param keystroke KeyStroke to associated
    * @param action The action to be executed
    * @return True if the keystroke was associated 
    */
-  def bindKeystrokeAction(keystroke: KeyStroke, action:Action):Boolean = {
-    val im=self.peer.getInputMap(javax.swing.JComponent.WHEN_IN_FOCUSED_WINDOW);
+  def bindKeystrokeAction(focus: FocusCondition.Value, keystroke: KeyStroke, action:Action):Boolean = {
+    val im=self.peer.getInputMap(focus.id);
     if(im!=null && keystroke!=null) {
       im.put(keystroke,action.title)
       self.peer.getActionMap.put(action.title,action.peer)
