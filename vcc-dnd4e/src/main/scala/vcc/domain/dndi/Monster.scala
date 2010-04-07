0def32f4662e0231b1bb2652333bb3e0942e625e
Thomas Santana <mailleux@gmail.com>
2010-04-07 01:09:23 +0000

    Added line breaks on generated Stat Block to improve user editing.

--- vcc-dnd4e/src/main/scala/vcc/domain/dndi/Monster.scala
+++ vcc-dnd4e/src/main/scala/vcc/domain/dndi/Monster.scala
@@ -18,258 +18,248 @@
 package vcc.domain.dndi
 
 import scala.util.matching.Regex
-import scala.xml.{Node,NodeSeq,Text=>XmlText,Elem}
 import vcc.domain.dndi.Monster.PowerDescriptionSupplement
 
 object Monster {
-  
+
   //Construction elements
-  case class Aura(name:String,desc:String) extends StatBlockDataSource {
-    def extract(key:String):Option[String] = {
-      val s:String = key.toUpperCase match {
+  case class Aura(name: String, desc: String) extends StatBlockDataSource {
+    def extract(key: String): Option[String] = {
+      val s: String = key.toUpperCase match {
         case "NAME" => name
         case "DESCRIPTION" => desc
         case _ => null
       }
-      if(s!=null) Some(s) else None
+      if (s != null) Some(s) else None
     }
-    def extractGroup(dontcare:String) = Nil
+
+    def extractGroup(dontcare: String) = Nil
   }
-  
-  //TODO: This is an ugly hack. Need to make this nicer.
-  @deprecated
-  private final val imageMap = Map(Parser.IconType.imageDirectory.map(x => (x._2,x._1)).toSeq: _*)
 
-  case class PowerDescriptionSupplement(emphasis:String,text:String) extends StatBlockDataSource {
-
-    def extract(key:String):Option[String] = {
-      val s:String = key.toUpperCase match {
+  case class PowerDescriptionSupplement(emphasis: String, text: String) extends StatBlockDataSource {
+    def extract(key: String): Option[String] = {
+      val s: String = key.toUpperCase match {
         case "HEADER" => emphasis
         case "DESCRIPTION" => text
         case _ => null
       }
-      if(s!=null) Some(s) else None
+      if (s != null) Some(s) else None
     }
 
-    def extractGroup(dontcare:String) = Nil
-  
+    def extractGroup(dontcare: String) = Nil
+
   }
 
-  case class Power(icon:Seq[Parser.IconType.Value],name:String,action: String, keywords:String) extends StatBlockDataSource {
+  case class Power(icon: Seq[Parser.IconType.Value], name: String, action: String, keywords: String) extends StatBlockDataSource {
+    private var _supplemnt: List[PowerDescriptionSupplement] = Nil
 
-    private var _supplemnt:List[PowerDescriptionSupplement] = Nil
-    
-    def extract(key:String):Option[String] = {
-      val s:String = key.toUpperCase match {
+    def extract(key: String): Option[String] = {
+      val s: String = key.toUpperCase match {
         case "NAME" => name
         case "DESCRIPTION" => description
         case "ACTION" => action
-        //FIXME
-        case "TYPE" if(icon!=null && !icon.isEmpty) => 
+        case "TYPE" if (icon != null && !icon.isEmpty) =>
           icon.map(i => Parser.IconType.iconToImage(i)).mkString(";")
         case "KEYWORDS" => keywords
         case _ => null
       }
-      if(s!=null) Some(s) else None
+      if (s != null) Some(s) else None
     }
 
-    def extractGroup(key:String) = if(key.toUpperCase == "POWER DESCRIPTION SUPPLEMENT") this._supplemnt.toSeq else Nil
+    def extractGroup(key: String) = if (key.toUpperCase == "POWER DESCRIPTION SUPPLEMENT") this._supplemnt.toSeq else Nil
 
     var description: String = null
 
-    def addDescriptionSupplement(sup:PowerDescriptionSupplement) {
+    def addDescriptionSupplement(sup: PowerDescriptionSupplement) {
       _supplemnt = _supplemnt ::: List(sup)
     }
 
-    override def toString:String = "Power("+icon+", "+name+", "+action+", "+keywords+", "+description+", "+this._supplemnt+")"
+    override def toString: String = "Power(" + icon + ", " + name + ", " + action + ", " + keywords + ", " + description + ", " + this._supplemnt + ")"
 
     def supplement = _supplemnt
-  }  
+  }
 
 }
 
 /**
  * Base monster load.
  */
-class Monster(val id:Int) extends DNDIObject with StatBlockDataSource {
-    
-  def extract(key:String):Option[String] = this(key.toUpperCase)
-  
-  def extractGroup(group:String) = group.toUpperCase match {
+class Monster(val id: Int) extends DNDIObject with StatBlockDataSource {
+  def extract(key: String): Option[String] = this(key.toUpperCase)
+
+  def extractGroup(group: String) = group.toUpperCase match {
     case "POWERS" => this.powers
     case "AURAS" => this.auras
   }
-  
-  import vcc.domain.dndi.Parser._
-  
-  private var _map=Map.empty[String,String]
-  private var _auras:List[Monster.Aura]=Nil
-  private var _power:List[Monster.Power]=Nil
-  
+
+  private var _map = Map.empty[String, String]
+  private var _auras: List[Monster.Aura] = Nil
+  private var _power: List[Monster.Power] = Nil
+
   def powers = _power
+
   def auras = _auras
 
-  def apply(attribute: String):Option[String] = {
-    if(_map.contains(attribute)) Some(_map(attribute))
+  def apply(attribute: String): Option[String] = {
+    if (_map.contains(attribute)) Some(_map(attribute))
     else None
   }
-  
-  private [dndi] def set(attribute:String,value:String) {
+
+  private[dndi] def set(attribute: String, value: String) {
     // Normalize some cases here:
     val normAttr = attribute.toUpperCase
-    val normValue:String = normAttr match {
-      case "HP" if(value.startsWith("1;")) => "1"
-      case "ROLE" if(value == "Minion") => "No Role"
+    val normValue: String = normAttr match {
+      case "HP" if (value.startsWith("1;")) => "1"
+      case "ROLE" if (value == "Minion") => "No Role"
       case _ => value
     }
     _map = _map + (normAttr -> normValue)
-  } 
-  
-  private [dndi] def addAura(name:String,desc:String):Monster.Aura = {
-    val a = new Monster.Aura(name,desc)
+  }
+
+  private[dndi] def addAura(name: String, desc: String): Monster.Aura = {
+    val a = new Monster.Aura(name, desc)
     _auras = _auras ::: List(a)
     a
-  } 
-  
-  protected[dndi] def addPower(icons:Seq[Parser.IconType.Value],name:String,action: String, keywords:String):Monster.Power = {
-    val p = Monster.Power(icons,name,action, keywords)
-    _power= _power ::: List(p)
+  }
+
+  protected[dndi] def addPower(icons: Seq[Parser.IconType.Value], name: String, action: String, keywords: String): Monster.Power = {
+    val p = Monster.Power(icons, name, action, keywords)
+    _power = _power ::: List(p)
     p
   }
-  
-  override def toString():String = {
-    "Monster["+id+"]("+_map+"; Aura="+_auras+"; powers="+_power+")"
+
+  override def toString(): String = {
+    "Monster[" + id + "](" + _map + "; Aura=" + _auras + "; powers=" + _power + ")"
   }
 }
 
 import Parser._
+
 trait BlockReader {
-  
-  def processBlock(block:BlockElement):Boolean
-  
-  def getObject:DNDIObject
+  def processBlock(block: BlockElement): Boolean
+
+  def getObject: DNDIObject
 }
 
-class MonsterBuilder(monster:Monster) extends BlockReader{
-  
-  final val reXP= new Regex("\\s*XP\\s*(\\d+)\\s*")
-  final val reLevel= new Regex("^\\s*Level\\s+(\\d+)\\s+(.*)$")
+class MonsterBuilder(monster: Monster) extends BlockReader {
+  final val reXP = new Regex("\\s*XP\\s*(\\d+)\\s*")
+  final val reLevel = new Regex("^\\s*Level\\s+(\\d+)\\s+(.*)$")
   final val reSecondary = """^Secondary Attack\s*(.*)\s*$""".r
-  final val primaryStats=Set("Initiative", "Senses", "HP", "Bloodied",
-                       		"AC", "Fortitude", "Reflex", "Will",
-                       		"Immune", "Resist", "Vulnerable",
-                       		"Saving Throws","Speed",  "Action Points")
-  
-  private var _lastPower:Monster.Power = null
+  final val primaryStats = Set("Initiative", "Senses", "HP", "Bloodied",
+    "AC", "Fortitude", "Reflex", "Will",
+    "Immune", "Resist", "Vulnerable",
+    "Saving Throws", "Speed", "Action Points")
 
-  private var _powerSupplement:String = null
-  
-  def getObject:DNDIObject = monster
+  private var _lastPower: Monster.Power = null
+
+  private var _powerSupplement: String = null
+
+  def getObject: DNDIObject = monster
 
   /**
    * Extract xp, level and role from compose text of the header
    */
-  private def normalizeTitle(l:List[(String,String)]): List[(String,String)] = {
+  private def normalizeTitle(l: List[(String, String)]): List[(String, String)] = {
     l match {
-      case ("xp",this.reXP(xp)):: rest => ("xp",xp) :: normalizeTitle(rest)
-      case ("level",this.reLevel(lvl,role)):: rest => ("level",lvl)::("role",role):: normalizeTitle(rest)
+      case ("xp", this.reXP(xp)) :: rest => ("xp", xp) :: normalizeTitle(rest)
+      case ("level", this.reLevel(lvl, role)) :: rest => ("level", lvl) :: ("role", role) :: normalizeTitle(rest)
       case p :: rest => p :: normalizeTitle(rest)
       case Nil => Nil
     }
   }
-  
+
   /**
    * Add key value pairs into the _map variable. Key will be turned into uppercase
    */
-  private def addToMap(items: List[(String,String)]) {
-    for((k,v)<- items) { monster.set(k,v) }
+  private def addToMap(items: List[(String, String)]) {
+    for ((k, v) <- items) {monster.set(k, v)}
   }
 
   /**
    * Primary block includes auras, that become pais, need to pull them out
    */
-  private def processPrimaryBlock(pairs: List[(String,String)]) {
-    for( (k,v)<- pairs ) {
-      if(primaryStats(k))  monster.set(k,v)
-      else monster.addAura(k,v)
-    }	
-  }
-
-  
-
-  object PowerHeader {
-    def unapply(parts:List[Part]):Option[(List[IconType.Value],String,String,String)] = {
-	  var p = breakToSpace(parts)
-	  var icons:List[IconType.Value] = Nil
-      
-	  while(!p.isEmpty && p.head.isInstanceOf[Icon]) {
-	    icons = p.head.asInstanceOf[Icon].itype :: icons
-        p = p.tail
-	  }
-   
-	  val name:String = p match {
-	    case Key(text) :: rest if(text != "Equipment") => // Need this check to avoid skipping equipments
-	      text
-	    case _ => return None 
-	  }
-	  p = p.tail
-	  val action:String = p match {
-	    case Text(text)::rest =>
-	      p = p.tail //Advance for last part
-	      text
-	    case _	=> null
-	  }
-	  val keywords:String = p match {
-	    case Icon(IconType.Separator) :: Key(text) :: Nil => text
-	    case Nil => null
-	    case _ => return None
-	  }
-      Some((icons,name,action,keywords))
+  private def processPrimaryBlock(pairs: List[(String, String)]) {
+    for ((k, v) <- pairs) {
+      if (primaryStats(k)) monster.set(k, v)
+      else monster.addAura(k, v)
     }
   }
 
-  def processBlock(block:BlockElement):Boolean = {
+
+
+  object PowerHeader {
+    def unapply(parts: List[Part]): Option[(List[IconType.Value], String, String, String)] = {
+      var p = breakToSpace(parts)
+      var icons: List[IconType.Value] = Nil
+
+      while (!p.isEmpty && p.head.isInstanceOf[Icon]) {
+        icons = p.head.asInstanceOf[Icon].itype :: icons
+        p = p.tail
+      }
+
+      val name: String = p match {
+        case Key(text) :: rest if (text != "Equipment") => // Need this check to avoid skipping equipments
+          text
+        case _ => return None
+      }
+      p = p.tail
+      val action: String = p match {
+        case Text(text) :: rest =>
+          p = p.tail //Advance for last part
+          text
+        case _ => null
+      }
+      val keywords: String = p match {
+        case Icon(IconType.Separator) :: Key(text) :: Nil => text
+        case Nil => null
+        case _ => return None
+      }
+      Some((icons, name, action, keywords))
+    }
+  }
+
+  def processBlock(block: BlockElement): Boolean = {
     block match {
-      case HeaderBlock("H1#monster",pairs) => addToMap(normalizeTitle(pairs))
-      case Block("P#flavor", parts @ Key("Initiative"):: _ ) =>
+      case HeaderBlock("H1#monster", pairs) => addToMap(normalizeTitle(pairs))
+      case Block("P#flavor", parts@Key("Initiative") :: _) =>
         processPrimaryBlock(partsToPairs(parts))
-      
+
       case Block("P#flavor alt", parts) =>
         parts match {
-          case PowerHeader(icons,name,action,keywords) =>
-            _lastPower = monster.addPower(icons,name,action,keywords)
+          case PowerHeader(icons, name, action, keywords) =>
+            _lastPower = monster.addPower(icons, name, action, keywords)
 
-          case Key("Equipment"):: SingleTextBreakToSpace(text) => 
-          	monster.set("Equipment",text)
-          
-          case Key("Alignment")::rest => 
-          	addToMap(partsToPairs(parts))
+          case Key("Equipment") :: SingleTextBreakToSpace(text) =>
+            monster.set("Equipment", text)
 
-          case s => throw new Exception("Failed to process!! "+s)
-          	return false
+          case Key("Alignment") :: rest =>
+            addToMap(partsToPairs(parts))
+
+          case s => throw new Exception("Failed to process!! " + s)
+          return false
         }
-      case Block("P#flavorIndent", SingleTextBreakToNewLine(text)) if(_lastPower != null)=> 
+      case Block("P#flavorIndent", SingleTextBreakToNewLine(text)) if (_lastPower != null) =>
         // This is a power description 
-        if(_powerSupplement != null) {
-          _lastPower.addDescriptionSupplement(PowerDescriptionSupplement(_powerSupplement,text))
+        if (_powerSupplement != null) {
+          _lastPower.addDescriptionSupplement(PowerDescriptionSupplement(_powerSupplement, text))
           _powerSupplement = null
         } else {
           _lastPower.description = text
         }
-      case Block("P#flavor",Emphasis(comment)::Nil) if(_lastPower != null) => 
-          // A secondary attack
-          _powerSupplement = comment
-          //_lastPower.secondaryKeywords = if(comment == "") null else comment
+      case Block("P#flavor", Emphasis(comment) :: Nil) if (_lastPower != null) =>
+        // A secondary attack
+        _powerSupplement = comment
+      //_lastPower.secondaryKeywords = if(comment == "") null else comment
 
-      case Block("P#flavor", parts @ Key("Description")::SingleTextBreakToNewLine(text)) => 
-        monster.set("Description",text)
-          
-      case Block("P#",Emphasis(text)::Nil) => monster.set("comment",(text))
+      case Block("P#flavor", parts@Key("Description") :: SingleTextBreakToNewLine(text)) =>
+        monster.set("Description", text)
+
+      case Block("P#", Emphasis(text) :: Nil) => monster.set("comment", (text))
       case NonBlock(dontcare) => // Don't care
       case s =>
-        throw new Exception("Failed to process: "+s)
-        return false  
+        throw new Exception("Failed to process: " + s)
+        return false
     }
     true
-  }  
+  }
 }
--- vcc-dnd4e/src/main/scala/vcc/domain/dndi/StatBlockBuilder.scala
+++ vcc-dnd4e/src/main/scala/vcc/domain/dndi/StatBlockBuilder.scala
@@ -1,4 +1,3 @@
-//$Id$
 /**
  * Copyright (C) 2008-2009 tms - Thomas Santana <tms@exnebula.org>
  *
@@ -15,53 +14,57 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-
+//$Id$
 
 package vcc.domain.dndi
 
-import scala.xml.{Elem,Node,Text}
+import scala.xml.{Elem, Node, Text}
 
 trait StatBlockDataSource {
-  def extract(string:String):Option[String]
-  def extractGroup(group:String):Seq[StatBlockDataSource]
+  def extract(string: String): Option[String]
+
+  def extractGroup(group: String): Seq[StatBlockDataSource]
 }
 
 object StatBlockBuilder {
+  val xmlLineBreak = scala.xml.Text("\n")
 
   abstract class Chunk {
-    def render(source:StatBlockDataSource):Seq[Node]
-    
-    def formatChunks(source:StatBlockDataSource)(parts:Chunk*):Seq[Node] = parts.map(part => part.render(source)).flatMap(x=>x)
-  
+    def render(source: StatBlockDataSource): Seq[Node]
+
+    def formatChunks(source: StatBlockDataSource)(parts: Chunk*): Seq[Node] = parts.map(part => part.render(source)).flatMap(x => x)
+
   }
-  
+
   case class ChunkGroup(chunks: Chunk*) extends Chunk {
-    def render(source:StatBlockDataSource):Seq[Node] = formatChunks(source)(chunks: _*)
+    def render(source: StatBlockDataSource): Seq[Node] = formatChunks(source)(chunks: _*)
   }
-  
+
   object Break extends Chunk {
-    def render(source:StatBlockDataSource) = Seq(<br/>)
+    def render(source: StatBlockDataSource) = Seq(<br/>, xmlLineBreak)
   }
-  
-  abstract class PartFormatter(key:String) extends Chunk {
-	def render(source:StatBlockDataSource):Seq[Node] = {
-	  val v = source.extract(key)
-	  if(v.isDefined) {
-        Seq((<b>{key}</b>),Text(format(v.get)))
-      } else Nil	}
-	def format(s:String):String
+
+  abstract class PartFormatter(key: String) extends Chunk {
+    def render(source: StatBlockDataSource): Seq[Node] = {
+      val v = source.extract(key)
+      if (v.isDefined) {
+        Seq((<b>{key}</b>), Text(format(v.get)))
+      } else Nil
+    }
+
+    def format(s: String): String
   }
-  
-  case class PairSpacedFmt(key:String) extends PartFormatter(key) {
-    def format(s:String) = s.formatted(" %s ")
+
+  case class PairSpacedFmt(key: String) extends PartFormatter(key) {
+    def format(s: String) = s.formatted(" %s ")
   }
-  
-  case class PairInitFmt(key:String) extends PartFormatter(key) {
-    def format(s:String) = try { s.toInt.formatted(" %+d ")} catch { case _ => s+" "}
+
+  case class PairInitFmt(key: String) extends PartFormatter(key) {
+    def format(s: String) = try {s.toInt.formatted(" %+d ")} catch {case _ => s + " "}
   }
-  
-  case class PairFlexFmt(key:String,fmt:String) extends PartFormatter(key) {
-    def format(s:String) = s.formatted(fmt)
+
+  case class PairFlexFmt(key: String, fmt: String) extends PartFormatter(key) {
+    def format(s: String) = s.formatted(fmt)
   }
 
   case class TextFormat(key: String, sep: Boolean) extends Chunk {
@@ -91,9 +94,7 @@ object StatBlockBuilder {
   case class BoldFormat(key: String, sep: Boolean) extends Chunk {
     def render(source: StatBlockDataSource): Seq[Node] = {
       val v = source.extract(key)
-      if (v.isDefined) (<b>
-        {v.get.formatted(if (sep) " %s " else "%s")}
-      </b>)
+      if (v.isDefined) (<b>{v.get.formatted(if (sep) " %s " else "%s")}</b>)
       else Nil
     }
   }
@@ -101,97 +102,106 @@ object StatBlockBuilder {
   case class EmphasisFormat(key: String, sep: Boolean) extends Chunk {
     def render(source: StatBlockDataSource): Seq[Node] = {
       val v = source.extract(key)
-      if (v.isDefined) (<i>
-        {v.get.formatted(if (sep) " %s " else "%s")}
-      </i>)
+      if (v.isDefined) (<i>{v.get.formatted(if (sep) " %s " else "%s")}</i>)
       else Nil
     }
   }
 
-  case class StaticXML(xml:Node) extends Chunk {
-    def render(source:StatBlockDataSource):Seq[Node] = Seq(xml)
+  case class StaticXML(xml: Node) extends Chunk {
+    def render(source: StatBlockDataSource): Seq[Node] = Seq(xml)
   }
-  
+
   case class Line(parts: Chunk*) extends Chunk {
-    override def render(source:StatBlockDataSource) = {
-      val s = formatChunks(source)(parts : _*)
-      if(!s.isEmpty) (s ++ <br/>)
+    override def render(source: StatBlockDataSource) = {
+      val s = formatChunks(source)(parts: _*)
+      if (!s.isEmpty) (s ++ <br/> ++ xmlLineBreak)
       else s
     }
   }
 
-  case class Para(pclass:String, parts: Chunk*) extends Chunk {
-    override def render(source:StatBlockDataSource) = (<p class={pclass}>{formatChunks(source)(parts : _*)}</p>)
+  case class Para(pclass: String, parts: Chunk*) extends Chunk {
+    override def render(source: StatBlockDataSource) = Seq(<p class={pclass}>{formatChunks(source)(parts: _*)}</p>, xmlLineBreak)
   }
 
-  case class Div(divClass:String, parts: Chunk*) extends Chunk {
-    override def render(source:StatBlockDataSource):Seq[Node] = (<div class={divClass}>{formatChunks(source)(parts : _*)}</div>)
+  case class Div(divClass: String, parts: Chunk*) extends Chunk {
+    override def render(source: StatBlockDataSource): Seq[Node] = (<div class={divClass}>
+      {formatChunks(source)(parts: _*)}
+    </div>)
   }
-  
-  case class IfDefined(key:String, parts: Chunk*) extends Chunk {
-     override def render(source:StatBlockDataSource):Seq[Node] = if(source.extract(key).isDefined) formatChunks(source)(parts: _*) else Nil
+
+  case class IfDefined(key: String, parts: Chunk*) extends Chunk {
+    override def render(source: StatBlockDataSource): Seq[Node] = if (source.extract(key).isDefined) formatChunks(source)(parts: _*) else Nil
   }
-  
-  case class Image(src:String) extends Chunk {
-    override def render(source:StatBlockDataSource):Seq[Node] = (<img src={src} />) 
+
+  case class Image(src: String) extends Chunk {
+    override def render(source: StatBlockDataSource): Seq[Node] = (<img src={src}/>)
   }
-  
-  case class ImageMap(key:String) extends Chunk {
-    def render(source:StatBlockDataSource):Seq[Node] = {
+
+  case class ImageMap(key: String) extends Chunk {
+    def render(source: StatBlockDataSource): Seq[Node] = {
       val v = source.extract(key)
-      if(v.isDefined) {
-        v.get.split(";").map{ src => (<img src={src}/>)}
+      if (v.isDefined) {
+        v.get.split(";").map {src => (<img src={src}/>)}
       }
       else Nil
     }
   }
-  
-  case class Group(group:String,chunks: Chunk*) extends Chunk {
-    def render(source:StatBlockDataSource):Seq[Node] = {
+
+  case class Group(group: String, chunks: Chunk*) extends Chunk {
+    def render(source: StatBlockDataSource): Seq[Node] = {
       val gds = source.extractGroup(group)
-      gds.map(subds => formatChunks(subds)(chunks: _*)).flatMap(x=>x)
+      gds.map(subds => formatChunks(subds)(chunks: _*)).flatMap(x => x)
     }
   }
-  
+
 }
 
-trait StatBlockBuilder  {
-  def generate(ds:StatBlockDataSource):Node
+trait StatBlockBuilder {
+  def generate(ds: StatBlockDataSource): Node
 }
 
 object MonsterStatBlockBuilder extends StatBlockBuilder {
   import StatBlockBuilder._
-  
+
   private val headBlock = Para("flavor",
-        Line(PairInitFmt("Initiative"),PairSpacedFmt("Senses")),
-        Group("Auras",Line(BoldFormat("Name",true),TextFormat("description",true))),
-          PairFlexFmt("HP"," %s; "),PairSpacedFmt("Bloodied"),Break,
-          Line(PairSpacedFmt("Regeneration")),
-          PairFlexFmt("AC"," %s; "),PairFlexFmt("Fortitude"," %s, "),PairFlexFmt("Reflex"," %s, "),PairSpacedFmt("Will"),Break,
-          Line(PairFlexFmt("Immune"," %s; "),PairFlexFmt("Resist"," %s; "),PairFlexFmt("Vulnerable"," %s; ")),
-          Line(PairInitFmt("Saving Throws")),Line(PairSpacedFmt("Speed")),Line(PairSpacedFmt("Action Points"))
-      ) 
+    Line(PairInitFmt("Initiative"), PairSpacedFmt("Senses")),
+    Group("Auras", Line(BoldFormat("Name", true), TextFormat("description", true))),
+    PairFlexFmt("HP", " %s; "), PairSpacedFmt("Bloodied"), Break,
+    Line(PairSpacedFmt("Regeneration")),
+    PairFlexFmt("AC", " %s; "), PairFlexFmt("Fortitude", " %s, "), PairFlexFmt("Reflex", " %s, "), PairSpacedFmt("Will"), Break,
+    Line(PairFlexFmt("Immune", " %s; "), PairFlexFmt("Resist", " %s; "), PairFlexFmt("Vulnerable", " %s; ")),
+    Line(PairInitFmt("Saving Throws")), Line(PairSpacedFmt("Speed")), Line(PairSpacedFmt("Action Points"))
+    )
   private val tailBlock = Para("alt flavor",
-        PairSpacedFmt("Alignment"),PairSpacedFmt("Languages"),Break,
-          Line(PairSpacedFmt("Skills")),
-          PairSpacedFmt("Str"),PairSpacedFmt("Dex"),PairSpacedFmt("Wis"),Break,
-          PairSpacedFmt("Con"),PairSpacedFmt("Int"),PairSpacedFmt("Cha"),Break,
-          Line(PairSpacedFmt("Equipment"))
-      )
+    PairSpacedFmt("Alignment"), PairSpacedFmt("Languages"), Break,
+    Line(PairSpacedFmt("Skills")),
+    PairSpacedFmt("Str"), PairSpacedFmt("Dex"), PairSpacedFmt("Wis"), Break,
+    PairSpacedFmt("Con"), PairSpacedFmt("Int"), PairSpacedFmt("Cha"), Break,
+    Line(PairSpacedFmt("Equipment"))
+    )
 
   private val powerBlock = Seq(
     Para("flavor alt", ImageMap("Type"), BoldFormat("Name", true), TextFormat("Action", true), IfDefined("Keywords", Image("x.gif"), BoldFormat("Keywords", true))),
-    Para("flavorIndent", MultiLineFormat("Description")), //TODO Handle line break for beholder
+    Para("flavorIndent", MultiLineFormat("Description")),
     Group("POWER DESCRIPTION SUPPLEMENT", Para("flavor", EmphasisFormat("header", false)), Para("flavorIndent", TextFormat("description", false)))
     )
 
 
-  def generate(ds:StatBlockDataSource) = {
-    val block = new ChunkGroup(headBlock,Group("Powers",powerBlock: _*),tailBlock)
-    (<html><head><link rel="stylesheet" type="text/css" href="dndi.css" /></head>
-     <body><div id="detail">
-      <h1 class="monster">{ds.extract("NAME").get}<span class="type">{ds.extract("TYPE").get + " " + ds.extract("ROLE").get}</span></h1>
-      { block.render(ds)}
-      </div></body></html>)
-    }
+  def generate(ds: StatBlockDataSource) = {
+    val block = new ChunkGroup(headBlock, Group("Powers", powerBlock: _*), tailBlock)
+    (<html>
+      <head>
+          <link rel="stylesheet" type="text/css" href="dndi.css"/>
+      </head>
+      <body>
+        <div id="detail">
+          <h1 class="monster">
+            {ds.extract("NAME").get}
+            <span class="type"> {ds.extract("TYPE").get + " " + ds.extract("ROLE").get}</span>
+          </h1>
+          {block.render(ds)}
+        </div>
+      </body>
+    </html>)
+  }
 }
