3d6edb9a7a289ed6fc8277556cc643bf2ded95ba
Thomas Santana <mailleux@gmail.com>
2010-05-03 09:04:54 +0000

    Fixed typo in model name. Added format function on ContainerComboBoxModel to improve output of objects.

--- vcc-dnd4e/src/main/scala/vcc/util/swing/ExplicitModelComboBox.scala
+++ vcc-dnd4e/src/main/scala/vcc/util/swing/ExplicitModelComboBox.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2008-2009 tms - Thomas Santana <tms@exnebula.org>
+ * Copyright (C) 2008-2010 - Thomas Santana <tms@exnebula.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,40 +18,43 @@
 package vcc.util.swing
 
 import scala.swing._
-import javax.swing.{AbstractListModel,ComboBoxModel}
+import javax.swing.{AbstractListModel, ComboBoxModel}
 
 /**
  * Model for ComboBox, it allows update to the contents via properties.
+ * @param iv Initial value for the contents of the model
+ * @param format A function that returns a string based on the value of A.
  */
-class ContainterComboBoxModel[A](iv:Seq[A]) extends AbstractListModel with ComboBoxModel {
-  
-  var entries:Seq[A]=iv
-  
-  private var selected:A = if(entries.isEmpty) null.asInstanceOf[A] else entries(0)
-  
+class ContainerComboBoxModel[A](iv: Seq[A], format: A => String) extends AbstractListModel with ComboBoxModel {
+  def this(iv: Seq[A]) = this (iv, null)
+
+  var entries: Seq[A] = iv
+
+  private var selected: A = if (entries.isEmpty) null.asInstanceOf[A] else entries(0)
+
   def getSelectedItem: AnyRef = selected.asInstanceOf[AnyRef]
-  
-  def setSelectedItem(a: Any) { selected = a.asInstanceOf[A] } 
-  
-  def getElementAt(n: Int) = entries(n).asInstanceOf[AnyRef]
-  
+
+  def setSelectedItem(a: Any) {selected = a.asInstanceOf[A]}
+
+  def getElementAt(n: Int) = (if (format != null) format(entries(n)) else entries(n)).asInstanceOf[AnyRef]
+
   def getSize = entries.length
-  
-  def contents_=(nv:Seq[A]) { 
-    entries=nv
-    fireIntervalAdded(this,0,nv.length)
+
+  def contents_=(nv: Seq[A]) {
+    entries = nv
+    fireIntervalAdded(this, 0, nv.length)
   }
-  
-  def contents:Seq[A]=entries
-  
+
+  def contents: Seq[A] = entries
+
 }
 
 /**
  * This sub class of scala.swing.ComboBox is designed to be constructed based on a model
  * and not on a list. This allows model to be changed externally.
  */
-class ExplicitModelComboBox[T](mdl:javax.swing.ComboBoxModel) extends ComboBox[T](Nil) {
+class ExplicitModelComboBox[T](mdl: javax.swing.ComboBoxModel) extends ComboBox[T](Nil) {
   import javax.swing._
   override lazy val peer: JComboBox = new JComboBox(mdl) with SuperMixin
-  
+
 }
