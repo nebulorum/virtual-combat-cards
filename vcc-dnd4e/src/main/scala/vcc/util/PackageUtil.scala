3be37032c2857770d2f463e4f3c3148c12af2b85
Thomas Santana <mailleux@gmail.com>
2010-09-05 20:21:22 +0000

    Fixed issue with MD5 starting with 0 being truncated.

--- vcc-dnd4e/src/main/scala/vcc/util/PackageUtil.scala
+++ vcc-dnd4e/src/main/scala/vcc/util/PackageUtil.scala
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 //$Id$
-
 package vcc.util
 
 import java.io._
@@ -23,81 +22,84 @@ import java.util.zip._
 import java.security.MessageDigest
 
 object PackageUtil {
-  
-  private val logger = org.slf4j.LoggerFactory.getLogger("infra") 
-  
-  def fileMD5Sum(file:File):String =  {
-	val digest = MessageDigest.getInstance("MD5");
-	val buffer = new Array[Byte](8192);
-	var read = 0;
-	val is=new FileInputStream(file)
-	var md5text:String = null
-	try {
-	  read = is.read(buffer)
-	  while( read  > 0) {
-		digest.update(buffer, 0, read);
-		read = is.read(buffer)
-	  }		
-	  val md5sum = digest.digest();
-	  val bigInt = new java.math.BigInteger(1, md5sum);
-	  md5text = bigInt.toString(16);
-	} catch {
-	  case e:java.io.IOException =>
-	    is.close()
-		throw new RuntimeException("Unable to process file for MD5", e);
-	}
-	try {
-		is.close();
-	} catch {
-	  case e:java.io.IOException =>
-			throw new RuntimeException("Unable to close input stream for MD5 calculation", e);
-	}
-	md5text
-  }
-  
-  def extractFilesFromZip(zipFile:File, destdir:File) {
+  private val logger = org.slf4j.LoggerFactory.getLogger("infra")
+
+  private val zeroFiller = (1 to 16).map(_ => "00").mkString("")
+
+  def fileMD5Sum(file: File): String = {
+    val digest = MessageDigest.getInstance("MD5");
+    val buffer = new Array[Byte](8192);
+    var read = 0;
+    val is = new FileInputStream(file)
+    var md5text: String = null
     try {
-      if(!destdir.exists && !destdir.isDirectory) {
-        throw new Exception("Bad directory "+destdir)
+      read = is.read(buffer)
+      while (read > 0) {
+        digest.update(buffer, 0, read);
+        read = is.read(buffer)
       }
-                          
-      
+      val md5sum = digest.digest();
+      val bigInt = new java.math.BigInteger(1, md5sum);
+      println(bigInt.toString(16))
+      md5text = zeroFiller + bigInt.toString(16)
+      md5text = md5text.substring(md5text.length - zeroFiller.length)
+    } catch {
+      case e: java.io.IOException =>
+        is.close()
+        throw new RuntimeException("Unable to process file for MD5", e);
+    }
+    try {
+      is.close();
+    } catch {
+      case e: java.io.IOException =>
+        throw new RuntimeException("Unable to close input stream for MD5 calculation", e);
+    }
+    md5text
+  }
+
+  def extractFilesFromZip(zipFile: File, destdir: File) {
+    try {
+      if (!destdir.exists && !destdir.isDirectory) {
+        throw new Exception("Bad directory " + destdir)
+      }
+
+
       val buf = new Array[Byte](1024)
-      var zipentry:ZipEntry = null
+      var zipentry: ZipEntry = null
       val zipinputstream = new ZipInputStream(new FileInputStream(zipFile))
 
       zipentry = zipinputstream.getNextEntry();
-      while (zipentry != null) { 
-    	//for each entry to be extracted
+      while (zipentry != null) {
+        //for each entry to be extracted
         val entryName = zipentry.getName()
-        logger.info("Extracting file: "+entryName)
-        var n:Int=0
+        logger.info("Extracting file: " + entryName)
+        var n: Int = 0
         val newFile = new File(entryName);
         val directory = newFile.getParent();
-        
-        val dest=new File(destdir,entryName)
-        if(zipentry.isDirectory()) {
-          logger.info("Creating directory: "+entryName)
-          if(!dest.exists) dest.mkdir()
-          if(dest.exists && !dest.isDirectory) throw new Exception("Zip contains directory "+entryName + " which is not a directory")
+
+        val dest = new File(destdir, entryName)
+        if (zipentry.isDirectory()) {
+          logger.info("Creating directory: " + entryName)
+          if (!dest.exists) dest.mkdir()
+          if (dest.exists && !dest.isDirectory) throw new Exception("Zip contains directory " + entryName + " which is not a directory")
         } else {
-          val fileoutputstream = new FileOutputStream(dest)             
-        
+          val fileoutputstream = new FileOutputStream(dest)
+
           n = zipinputstream.read(buf, 0, 1024)
           while ((n) > -1) {
             fileoutputstream.write(buf, 0, n);
             n = zipinputstream.read(buf, 0, 1024)
           }
-          fileoutputstream.close(); 
+          fileoutputstream.close();
         }
         zipinputstream.closeEntry();
         zipentry = zipinputstream.getNextEntry();
-      }//while
+      } //while
 
       zipinputstream.close();
-   } catch { 
-     case e:Exception =>
-     	e.printStackTrace();
-   }
+    } catch {
+      case e: Exception =>
+        e.printStackTrace();
+    }
   }
 }
