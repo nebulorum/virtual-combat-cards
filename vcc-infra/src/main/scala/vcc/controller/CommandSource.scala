0c8617c8154f891827870ffda8fef673d23e8949
Thomas Santana <mailleux@gmail.com>
2010-10-26 01:48:41 +0000

    Added logic to collect possible Rulings, send them the use and process Ruling Decision logic.

--- vcc-infra/src/main/scala/vcc/controller/CommandSource.scala
+++ vcc-infra/src/main/scala/vcc/controller/CommandSource.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2008-2010 - Thomas Santana <tms@exnebula.org>
+ *    Copyright (C) 2008-2010 - Thomas Santana <tms@exnebula.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,11 +15,67 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 //$Id$
-
 package vcc.controller
 
+import message.TransactionalAction
+
+/**
+ * A ruling is some aspect of the system that cannot be decided by the computer and need some form of human decisions.
+ * It is used to get information from the user prior to executing actions. It is assumed that a Ruling will itself
+ * generate TransactionalAction. All Ruling have answer/decision type.
+ */
+trait Ruling[A] {
+
+  /**
+   * This method must generate a list actions that based on a given decision. The list may be empty, but may not be null.
+   * For any given {@code decision} any evocation of this method should produce the same list of actions (idempotent).
+   * @param decision The value of the decision
+   * @return a List of action that represent the effects of a Ruling on the system.
+   */
+  protected[controller] def generateActions(decision: A): List[TransactionalAction]
+
+  /**
+   * Helper function that will return a Decision wrapping this ruling and a valid answer to it.
+   */
+  def makeDecision(decision: A): Decision[A] = new Decision(decision, this)
+}
+
+/**
+ * Decision is a simple class to collect the Decision on a Ruling and it's original ruling.
+ */
+class Decision[A](val decision: A, val decisionFor: Ruling[A]) {
+
+  /**
+   * This method will access the Ruling's implementation of the action generator, and produce a ser
+   */
+  def generateRulingActions(): List[TransactionalAction] = decisionFor.generateActions(decision)
+}
+
+/**
+ * CommandSource represents the entity that sent the TransactionalAction to the Tracker. It will be used
+ * to report success or failure of the execution of an action, and to allow the TransactionalProcessors to query the
+ * source for ruling on things the tracker cannot resolve by itself.
+ */
 trait CommandSource {
+
+  /**
+   * This method is invoked to indicate that a given actions was successfully executed.
+   * @param msg Information on what was executed (normally the TransactionalAction message)
+   * @param producedChanges Indicates if the Transaction contains changes to the current state of the system.
+   */
   def actionCompleted(msg: String, producedChanges: Boolean)
 
+  /**
+   * Indicates that the action being executed failed to complete, normally due to exceptions.
+   * @param reason Description of the problem that caused the action to fail.
+   */
   def actionCancelled(reason: String)
+
+  /**
+   * This method is used to query the CommandSource for ruling on issues that the tracker cannot resolve by itself.
+   * @param rulings Item that require ruling by the CommandSource, these can be seen as questions that need answers.
+   * @return The return must include a single Decision for each Ruling in the order they were provided in the
+   * {@code ruling} parameter. 
+   */
+  def provideDecisionsForRulings(rulings: List[Ruling[_]]): List[Decision[_]] = Nil
 }
--- vcc-infra/src/main/scala/vcc/controller/TransactionalProcessor.scala
+++ vcc-infra/src/main/scala/vcc/controller/TransactionalProcessor.scala
@@ -22,6 +22,14 @@ import vcc.controller.message.TransactionalAction
 import scala.collection.mutable.Queue
 
 /**
+ * This exceptions is used to indicate that the CommandSource failed to return a decision for all rulings requested. This
+ * may also occur if decisions are supplied in the wrong order.
+ * @see TransactionalProcessor.queryCommandSource
+ * @param ruling The missing ruling
+ */
+class MissingDecisionException(ruling: Ruling[_]) extends Exception("Missing Decision for Ruling: " + ruling)
+
+/**
  * This exception should be thrown when the action requested cant be executed
  * @param msg Reason fro the Illegality
  */
@@ -63,8 +71,14 @@ abstract class TransactionalProcessor[C](val context: C, aQueue: Queue[Transacti
    */
   private var rewriteRules: List[PartialFunction[TransactionalAction, Seq[TransactionalAction]]] = Nil
 
+
   /**
-   * Add a handler to the processor, all handlers that apply will be executed while
+   * List of Ruling search partial functions
+   */
+  private var rulingSearch: List[PartialFunction[TransactionalAction, List[Ruling[_]]]] = Nil
+
+  /**
+   *  Add a handler to the processor, all handlers that apply will be executed while
    * processing a TransactionalAction.
    * @param handler A partial function on TransactionalAction
    */
@@ -72,6 +86,12 @@ abstract class TransactionalProcessor[C](val context: C, aQueue: Queue[Transacti
     handlers = handlers ::: List(handler)
   }
 
+  /**
+   * Adds a rewrite rule which can change a TransactionalAction into other actions according to need. These rules are
+   * called when the message is dispatched. Only the first rule is invoked. TransactionalAction that have been generated
+   * by the rule will NOT be processed again by rewrite rules. If no rule applies the original message is enqueued.
+   * @param rule The rule that take a single TransactionalAction and rewrites to a sequence of actions.
+   */
   protected def addRewriteRule(rule: PartialFunction[TransactionalAction, Seq[TransactionalAction]]) {
     rewriteRules = rewriteRules ::: List(rule)
   }
@@ -82,6 +102,41 @@ abstract class TransactionalProcessor[C](val context: C, aQueue: Queue[Transacti
    */
   protected def enqueueAction(action: TransactionalAction) {
     msgQueue.enqueue(action)
+  }
+
+  /**
+   * Adds a ruling search to the processor, these partial function will be called to check if the current action being
+   * dispatched requires ruling prior to it's execution. All searches will be applied and ruling generated added to a
+   * single set of requests to the CommandSource. The Decisions will then be used to generate a set of TransactionalActions
+   * that will be invoked prior to the original TransactionalAction. No further search for ruling will be applied on these
+   * TransactionalActions generated.
+   * @param search Partial function that will be checked for generation of Rulings 
+   */
+  protected def addRulingSearch(search: PartialFunction[TransactionalAction, List[Ruling[_]]]) {
+    rulingSearch = rulingSearch ::: List(search)
+  }
+
+
+  /**
+   * This helper function is used to quere CommandSource for decisions on rulings. It makes sure decisions come in the
+   * correct order and for all rulings required, then generates all TransactionalAction related to the decisions
+   * @param source CommandSource to be queried for decisions, this will normally map to the user interface.
+   * @param rulings List of issues that need ruling
+   * @return List of TransactionalAction generated concatenating each Decision.generateRulingActions all the decisions.
+   */
+  private[controller] def queryCommandSource(source: CommandSource, rulings: List[Ruling[_]]): List[TransactionalAction] = {
+    val decisions = source.provideDecisionsForRulings(rulings)
+    val zipped = rulings zip decisions
+    for ((q, d) <- zipped) {
+      if (q != d.decisionFor)
+        throw new MissingDecisionException(q)
+    }
+    //Less answers than questions
+    if (zipped.size < rulings.size) {
+      throw new MissingDecisionException(rulings(zipped.size))
+    }
+
+    decisions.flatMap(d => d.generateRulingActions())
   }
 
   /**
@@ -100,16 +155,27 @@ abstract class TransactionalProcessor[C](val context: C, aQueue: Queue[Transacti
     this.source = source
     try {
       while (!msgQueue.isEmpty) {
-        //TODO: Peek message and check for necessary rulings.
-        val msg = msgQueue.dequeue
-        var handled = false
-        for (hndl <- handlers) {
-          if (hndl.isDefinedAt(msg)) {
-            handled = true
-            hndl.apply(msg)
-          }
+        //Expand actions to include ruled actions, this will also dequeue the main message
+        val ruledActions: List[TransactionalAction] = {
+          val msg = msgQueue.dequeue
+          val rulings = rulingSearch.flatMap(search => if (search.isDefinedAt(msg)) search(msg) else Nil)
+          if (!rulings.isEmpty)
+            queryCommandSource(source, rulings) ::: List(msg)
+          else
+            List(msg)
         }
-        if (!handled) throw new UnhandledActionException(msg)
+
+        //Loop through ruled actions
+        for (message <- ruledActions) {
+          var handled = false
+          for (hndl <- handlers) {
+            if (hndl.isDefinedAt(message)) {
+              handled = true
+              hndl.apply(message)
+            }
+          }
+          if (!handled) throw new UnhandledActionException(message)
+        }
       }
     } catch {
       // We had an exception, flush message buffer to avoid leaving trash messages
@@ -119,5 +185,4 @@ abstract class TransactionalProcessor[C](val context: C, aQueue: Queue[Transacti
     }
     trans = null
   }
-
 }
