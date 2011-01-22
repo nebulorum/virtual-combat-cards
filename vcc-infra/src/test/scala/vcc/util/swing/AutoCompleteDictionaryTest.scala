a9ca94c659053510e05e384435769d05c2b4558f
Thomas Santana <mailleux@gmail.com>
2011-01-22 11:52:41 +0000

    Updated Id field

--- vcc-infra/src/test/scala/vcc/util/swing/AutoCompleteDictionaryTest.scala
+++ vcc-infra/src/test/scala/vcc/util/swing/AutoCompleteDictionaryTest.scala
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-//$Id
+//$Id$
 package vcc.util.swing
 
 import org.specs.SpecificationWithJUnit
@@ -24,7 +24,7 @@ import org.specs.mock.Mockito
 class AutoCompleteDictionaryTest extends SpecificationWithJUnit with Mockito {
 
   "AutoCompleteDictionary class" should {
-    val dict = new AutoCompleteDictionary(List("","test","tea"))
+    val dict = new AutoCompleteDictionary(List("", "test", "tea"))
 
     "return blank prefix is need" in {
       dict.findSuggestion("") must_== Some("")
@@ -44,51 +44,51 @@ class AutoCompleteDictionaryTest extends SpecificationWithJUnit with Mockito {
   }
 
   "AutoCompleteDictionary object" should {
-    val callback = mock[(String,String)=>Unit]
+    val callback = mock[(String, String) => Unit]
 
     "ignore blank lines and \\r" in {
       val winBuffer = new ByteArrayInputStream("first\n\rsecond\n\rsector\n\r ".getBytes())
 
-      val dict = AutoCompleteDictionary.fromStream(winBuffer,callback)
-      there was no(callback).apply(any[String],any[String])
+      val dict = AutoCompleteDictionary.fromStream(winBuffer, callback)
+      there was no(callback).apply(any[String], any[String])
       dict.findSuggestion(" ") must_== None
       dict.findSuggestion("") must_== Some("first")
     }
 
     "ignore blank lines work with linux format" in {
       val linuxBuffer = new ByteArrayInputStream("first\nsecond\nsector\n ".getBytes())
-      val dict = AutoCompleteDictionary.fromStream(linuxBuffer,callback)
-      there was no(callback).apply(any[String],any[String])
+      val dict = AutoCompleteDictionary.fromStream(linuxBuffer, callback)
+      there was no(callback).apply(any[String], any[String])
       dict.findSuggestion(" ") must_== None
       dict.findSuggestion("") must_== Some("first")
     }
 
     "remove extra spaced on the line" in {
       val toTrim = new ByteArrayInputStream("   first  \n  second  \n  sector   \n".getBytes())
-      val dict = AutoCompleteDictionary.fromStream(toTrim,callback)
-      there was no(callback).apply(any[String],any[String])
+      val dict = AutoCompleteDictionary.fromStream(toTrim, callback)
+      there was no(callback).apply(any[String], any[String])
       dict.findSuggestion("fi") must_== Some("first")
       dict.findSuggestion("se") must_== Some("second")
     }
 
     "warn if compound term has same prefix" in {
       val toRead = new ByteArrayInputStream("  second  \n  second attack  \n".getBytes())
-      val dict = AutoCompleteDictionary.fromStream(toRead,callback)
-      there was one(callback).apply("second attack","Prefix 'second' already defined")
+      val dict = AutoCompleteDictionary.fromStream(toRead, callback)
+      there was one(callback).apply("second attack", "Prefix 'second' already defined")
       dict.findSuggestion("se") must_== Some("second")
       dict.findSuggestion("second at") must_== Some("second attack")
     }
 
     "warn if compound term has same prefix in reverse" in {
       val toRead = new ByteArrayInputStream("  second attack \n  second  \n".getBytes())
-      val dict = AutoCompleteDictionary.fromStream(toRead,callback)
-      there was one(callback).apply("second","Prefix 'second' already defined")
+      val dict = AutoCompleteDictionary.fromStream(toRead, callback)
+      there was one(callback).apply("second", "Prefix 'second' already defined")
       dict.findSuggestion("se") must_== Some("second attack")
     }
 
     "ignore comment lines" in {
       val toRead = new ByteArrayInputStream("# comment\n  second  \n".getBytes())
-      val dict = AutoCompleteDictionary.fromStream(toRead,callback)
+      val dict = AutoCompleteDictionary.fromStream(toRead, callback)
       dict.findSuggestion("# co") must_== None
     }
   }
