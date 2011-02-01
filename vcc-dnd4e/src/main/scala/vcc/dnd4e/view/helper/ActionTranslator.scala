9eaa6485f7a272aa63ff20fc9145bd8e1f0e6e6d
Thomas Santana <mailleux@gmail.com>
2011-02-01 16:19:47 +0000

    Create ActionTranslator to provide human readable messages based on a TransactionalAction

---
+++ vcc-dnd4e/src/main/scala/vcc/dnd4e/view/helper/ActionTranslator.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2008-2011 - Thomas Santana <tms@exnebula.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+//$Id$
+package vcc.dnd4e.view.helper
+
+import vcc.controller.message.TransactionalAction
+import vcc.dnd4e.domain.tracker.common.Command.InitiativeAction
+import vcc.dnd4e.domain.tracker.common.{InitiativeTracker, CombatStateView}
+
+object ActionTranslator {
+  private def formatIfNotNull(s: String, fmt: String) = if (s != null) s.formatted(fmt) else ""
+
+  def fullActionMessage(state: CombatStateView, action: TransactionalAction): String = {
+    action match {
+      case InitiativeAction(ioi, InitiativeTracker.action.StartRound) =>
+        val cmb = state.combatantViewFromID(ioi.combId)
+        "Start round of " + cmb.definition.entity.name + formatIfNotNull(cmb.definition.alias, " - %s") + " [" + ioi.toLabelString + "]"
+      case InitiativeAction(ioi, InitiativeTracker.action.EndRound) =>
+        val cmb = state.combatantViewFromID(ioi.combId)
+        "Start round of " + cmb.definition.entity.name + formatIfNotNull(cmb.definition.alias, " - %s") + " [" + ioi.toLabelString + "]"
+      case _ => action.description
+    }
+  }
+}
---
+++ vcc-dnd4e/src/test/scala/vcc/dnd4e/view/helper/ActionTranslatorTest.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2008-2011 - Thomas Santana <tms@exnebula.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+//$Id$
+package vcc.dnd4e.view.helper
+
+import org.specs.SpecificationWithJUnit
+import org.specs.mock.Mockito
+import vcc.dnd4e.domain.tracker.common.Command.{InitiativeAction, StartCombat}
+import vcc.dnd4e.domain.tracker.common.InitiativeTracker._
+import vcc.controller.message.TransactionalAction
+import vcc.dnd4e.domain.tracker.common._
+import vcc.dnd4e.model.CombatantEntity
+import vcc.dnd4e.model.common.CombatantType
+
+class ActionTranslatorTest extends SpecificationWithJUnit with Mockito {
+
+  val combA = CombatantID("A")
+  val combB = CombatantID("B")
+  val ioiA = InitiativeOrderID(combA, 0)
+  val ioiB = InitiativeOrderID(combB, 0)
+  val mState = mock[CombatStateView]
+  val mCombA = mock[CombatantStateView]
+  val mCombB = mock[CombatantStateView]
+  mState.combatantViewFromID(combA) returns mCombA
+  mState.combatantViewFromID(combB) returns mCombB
+  val combADef = combatantDefinition(combA, "Goblin", "Shorty", CombatantType.Monster)
+  val combBDef = combatantDefinition(combB, "Goblin", null, CombatantType.Monster)
+  mCombA.definition returns combADef
+  mCombB.definition returns combBDef
+
+  "ActionTranslator" should {
+    "fallback to description" in {
+      val act = mock[TransactionalAction]
+      val desc = "something " + System.currentTimeMillis
+      act.description returns desc
+      ActionTranslator.fullActionMessage(mState, act) must_== desc
+    }
+
+    "tranlate StartRound with alias" in {
+      ActionTranslator.fullActionMessage(mState, InitiativeAction(ioiA, action.StartRound)) must_== "Start round of Goblin - Shorty [Aº]"
+    }
+
+    "tranlate StartRound without alias" in {
+      ActionTranslator.fullActionMessage(mState, InitiativeAction(ioiB, action.StartRound)) must_== "Start round of Goblin [Bº]"
+    }
+
+    "tranlate EndRound with alias" in {
+      ActionTranslator.fullActionMessage(mState, InitiativeAction(ioiA, action.EndRound)) must_== "Start round of Goblin - Shorty [Aº]"
+    }
+
+    "tranlate EndRound without alias" in {
+      ActionTranslator.fullActionMessage(mState, InitiativeAction(ioiB, action.EndRound)) must_== "Start round of Goblin [Bº]"
+    }
+  }
+
+  def combatantDefinition(comb: CombatantID, name: String, alias: String, ctype: CombatantType.Value) =
+    CombatantRosterDefinition(comb, alias, CombatantEntity(null, name, null, 0, ctype, null))
+
+}
