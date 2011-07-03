32d3ed4c805c9ef7b424008c9dbf93078b6f97a6
Thomas Santana <mailleux@gmail.com>
2011-07-03 02:57:52 +0000

    Reimplemented auto-start start dead.

--- vcc-dnd4e/src/main/scala/vcc/dnd4e/tracker/dispatcher/MigrationHandler.scala
+++ vcc-dnd4e/src/main/scala/vcc/dnd4e/tracker/dispatcher/MigrationHandler.scala
@@ -21,6 +21,7 @@ import vcc.dnd4e.domain.tracker.transactional.AbstractCombatController
 import vcc.dnd4e.tracker.common._
 import vcc.dnd4e.domain.tracker.common.Command._
 import InitiativeTracker.action
+import java.io.PrintStream
 
 trait MigrationHandler {
   this: AbstractCombatController =>
@@ -35,12 +36,38 @@ trait MigrationHandler {
     case InitiativeAction(it, action) => Seq(InternalInitiativeAction(it, action))
   }
 
+  def dumpState(state: CombatState, os: PrintStream) {
+    os.println("\tOrder: " + state.order)
+    os.println("\tCombs: " + state.roster)
+  }
 
   addHandler {
     case action =>
       val ts = ActionTranslator.translate(action)
-      println("Action: " + action + "Mapped to: " + ts.mkString(" + "))
+      println("Action: " + action + "\nMapped to: " + ts.mkString(" + "))
+      val oldState = context.iState.value
       context.iState.value = context.iState.value.transitionWith(ts)
-      println("New State: " + context.iState.value)
+      println("   New State: ")
+      dumpState(context.iState.value, System.out)
+      // Check if we need to advance dead if we rotated
+      if (oldState.order.nextUp != context.iState.value.order.nextUp) {
+        //We know we have someone new as the nextUp (first in order)
+        //Now check if we just ended some one else and auto start if next is dead
+        action match {
+          case InternalInitiativeAction(_, initAction) if ((initAction == InitiativeTracker.action.EndRound || initAction == InitiativeTracker.action.Delay)) => {
+            val state = context.iState.value
+            val nextIOI = state.order.nextUp.get
+            val nextIT = state.order.tracker(nextIOI)
+            val health = context.combatantFromID(nextIOI.combId).health
+            if (health.status == HealthTracker.Status.Dead) {
+              if (nextIT.state == InitiativeTracker.state.Delaying)
+                enqueueAction(InternalInitiativeAction(nextIT.orderID, InitiativeTracker.action.EndRound))
+              enqueueAction(InternalInitiativeAction(nextIT.orderID, InitiativeTracker.action.StartRound))
+              enqueueAction(InternalInitiativeAction(nextIT.orderID, InitiativeTracker.action.EndRound))
+            }
+          }
+          case _ =>
+        }
+      }
   }
 }
