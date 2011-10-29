46dee7aeac98602f496f5a4f355f14c527f0062b
Thomas Santana <mailleux@gmail.com>
2011-10-28 22:07:28 -0200

    Removed RulingHandler and RulingSearchService since functionality migrated to the new system.

--- vcc-dnd4e/src/main/scala/vcc/dnd4e/domain/tracker/transactional/CombatController.scala
+++ vcc-dnd4e/src/main/scala/vcc/dnd4e/domain/tracker/transactional/CombatController.scala
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2008-2010 - Thomas Santana <tms@exnebula.org>
+/*
+ * Copyright (C) 2008-2011 - Thomas Santana <tms@exnebula.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-//$Id$
 package vcc.dnd4e.domain.tracker.transactional
 
 import vcc.controller.TransactionalProcessor
@@ -49,6 +48,6 @@ abstract class AbstractCombatController(val rules: CombatStateRules, state: Comb
  * This is the final combat controller that includes all action handlers
  */
 class CombatController(rules: CombatStateRules, state: CombatContext, queue: Queue[TransactionalAction])
-  extends AbstractCombatController(rules, state, queue) with MigrationHandler with RulingHandler {
+  extends AbstractCombatController(rules, state, queue) with MigrationHandler {
   def this(rules: CombatStateRules, state: CombatContext) = this (rules, state, new Queue[TransactionalAction]())
 }
