1843e6e20ed91d06378d2734f79bb0a830afc3b5
Thomas Santana <mailleux@gmail.com>
2013-02-07 22:28:39 -0200

    Added simple PlayerView to test long polling for combat state.

---
+++ vcc-web/src/main/webapp/js/player.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2008-2013 - Thomas Santana <tms@exnebula.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+function FetchCtrl($scope, $http) {
+  $scope.method = 'GET';
+  $scope.url = 'player-view';
+
+  $scope.fetch = function() {
+    $scope.code = null;
+    $scope.response = null;
+
+    $http({method: $scope.method, url: $scope.url, cache: false}).
+      success(function(data, status) {
+        $scope.status = status;
+        $scope.data = data;
+        $scope.fetch();
+      }).
+      error(function(data, status) {
+        $scope.data = data || "Request failed";
+        $scope.status = status;
+    });
+  };
+
+  $scope.updateModel = function(method, url) {
+    $scope.method = method;
+    $scope.url = url;
+  };
+}
