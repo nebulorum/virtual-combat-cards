92733617aee98cc33da9cf531af2413f0272b414
Thomas Santana <mailleux@gmail.com>
2015-02-08 13:44:15 -0200

    Fixed generation of library list.

--- distribution/src/main/build-library-list.sh
+++ distribution/src/main/build-library-list.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-pwd
-cd ../../vcc-main
-
 if [ `dirname $0` != "distribution/src/main" ]; then
-    echo "Run from base build directory"
+    echo "Run from base build directory, where the main pom.xml is housed"
     exit 1
 fi
 
+
 cd vcc-main
-mvn dependency:list -Pvcc-main -DincludeScope=runtime | grep compile | awk -F ":" '{ print $2 ".jar" }' > ../distribution/src/main/resources
+TEMP=../distribution/target/library-list.building
+TARGET=../distribution/src/main/resources/library.lst
+
+echo "vcc-main.jar" > ${TEMP}
+mvn dependency:list -Pvcc-main -DincludeScope=runtime | grep compile | awk -F ":" '{ print $2 ".jar" }' >> ${TEMP}
+
+#echo $TARGET
+sort ${TEMP}  > ${TARGET}
+rm ${TEMP}
+echo "Create ${TARGET}."
